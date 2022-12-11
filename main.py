1ddb31f2b88dfe19ea37d6f8e0e67fae07b09417
Piop2 <seungyounyou0220@gmail.com>
2022-12-11 16:19:22 +0900

    add config: presence setting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from discord import Activity
+from discord import ActivityType
 from discord import Intents
 from discord import Status
 from discord.ext import commands
@@ -8,7 +9,6 @@ from utils.logger import get_logger
 
 
 root_log = get_logger("root")
-
 
 class Bot(commands.Bot):
     def __init__(self):
@@ -24,13 +24,47 @@ class Bot(commands.Bot):
         return
 
     async def on_ready(self):
-        root_log.info(f"activity set: name={self.activity.name} type={self.activity.type}")
-        root_log.info(f"status set: {self.status}")
+        presence = conf["presence"]
+        activity_name = presence["activity"]["name"]
+        match presence["activity"]["type"]:
+            case 1:
+                activity_type = ActivityType.unknown
+            case 2:
+                activity_type = ActivityType.playing
+            case 3:
+                activity_type = ActivityType.streaming
+            case 4:
+                activity_type = ActivityType.listening
+            case 5:
+                activity_type = ActivityType.watching
+            case 6:
+                activity_type = ActivityType.custom
+            case 7:
+                activity_type = ActivityType.competing
+            case _:
+                activity_type = ActivityType.unknown
+
+        match presence["status"]["type"]:
+            case 1:
+                status_type = Status.online
+            case 2:
+                status_type = Status.offline
+            case 3:
+                status_type = Status.idle
+            case 4:
+                status_type = Status.do_not_disturb
+            case 5:
+                status_type = Status.invisible
+            case _:
+                status_type = Status.online
+
+        root_log.info(f"activity set: name={activity_name} type={activity_type}")
+        root_log.info(f"status set: type={status_type}")
         root_log.info("ready")
-        await self.change_presence(activity=Activity(name="문제 풀이", type=2), status=Status.do_not_disturb)
+        await self.change_presence(activity=Activity(name=activity_name, type=activity_type), status=status_type)
         return
 
 
 if __name__ == "__main__":
-    token = conf["bot"]["token"]
+    token = conf["token"]
     Bot().run(token=token)
