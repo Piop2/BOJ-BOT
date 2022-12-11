52f70c8eb640c17ddcba4df8ad5450d6b528086e
Piop2 <seungyounyou0220@gmail.com>
2022-12-12 01:35:57 +0900

    update message send to embed style

--- cogs/problem.py
+++ cogs/problem.py
@@ -3,11 +3,12 @@
 : /problem problem_id
 
 # Param
-int problem_id: BOJ에 등록된 문제 id
+int problem_id: BOJ 에 등록된 문제 id
 """
 from discord import app_commands
-from discord import Interaction
 from discord.ext import commands
+from discord import Embed
+from discord import Interaction
 
 from boj.api.problem import search_problem
 from boj.error import BOJApiError
@@ -21,7 +22,7 @@ class SearchProblem(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.command(name="problem", description="search boj problem with id")
+    @app_commands.command(name="problem", description="search BOJ problem with ID")
     @app_commands.describe(problem_id="problem ID registered on BOJ")
     async def search(self, interaction: Interaction, problem_id: int) -> None:
         try:
@@ -29,9 +30,14 @@ class SearchProblem(commands.Cog):
         except BOJApiError.ProblemApiError.ProblemNotExistError:
             await interaction.response.send_message(f"ERROR problem id '{problem_id}' does not exist", ephemeral=False)
             problem_log.warning(f"problem does not exist: {problem_id}")
-        else:
-            await interaction.response.send_message(f"{problem.problem_id}.{problem.title_ko}: {problem.rank}", ephemeral=False)
-            problem_log.info(f"problem found: {problem_id}")
+            return
+
+        embed = Embed(title=f"{problem.id}. {problem.title}", url=problem.url)
+        embed.set_author(name=problem.rank , url=f"https://static.solved.ac/tier_small/{problem.level}.svg")
+        embed.set_footer(text="".join([f"#{i} " for i in problem.shorts]))
+
+        await interaction.response.send_message(embed = embed, ephemeral=False)
+        problem_log.info(f"problem found: {problem_id}")
         return
 
 
