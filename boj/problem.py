335b033e3bd5256e7c6fbed305c1f1c244001515
Piop2 <seungyounyou0220@gmail.com>
2022-12-11 12:13:33 +0900

    problem.get_rank -> utils.rank.get_rank

--- boj/problem.py
+++ boj/problem.py
@@ -1,42 +1,4 @@
-def get_rank(level: int) -> str:
-    rank = {
-        0: "Unrated",
-        1: "Bronze V",
-        2: "Bronze IV",
-        3: "Bronze III",
-        4: "Bronze II",
-        5: "Bronze I",
-        6: "Silver V",
-        7: "Silver IV",
-        8: "Silver III",
-        9: "Silver II",
-        10: "Silver I",
-        11: "Gold V",
-        12: "Gold IV",
-        13: "Gold III",
-        14: "Gold II",
-        15: "Gold I",
-        16: "Platinum V",
-        17: "Platinum IV",
-        18: "Platinum III",
-        19: "Platinum II",
-        20: "Platinum I",
-        21: "Diamond V",
-        22: "Diamond IV",
-        23: "Diamond III",
-        24: "Diamond II",
-        25: "Diamond I",
-        26: "Ruby V",
-        27: "Ruby IV",
-        28: "Ruby III",
-        29: "Ruby II",
-        30: "Ruby I",
-    }
-    return rank[level]
-
-
-def get_tier_image_url(level: int) -> str:
-    return f"https://static.solved.ac/tier_small/{level}.svg"
+from boj.utils.rank import get_rank
 
 
 def get_problem_url(problem_id: int) -> str:
@@ -45,15 +7,15 @@ def get_problem_url(problem_id: int) -> str:
 
 class Problem:
     def __init__(
-        self,
-        problem_id: int,
-        title_ko: str,
-        is_solvable: bool,
-        is_partial: bool,
-        accepted_user_count: int,
-        level: int,
-        average_tries: int,
-        ko_shorts: list[str, ...],
+            self,
+            problem_id: int,
+            title_ko: str,
+            is_solvable: bool,
+            is_partial: bool,
+            accepted_user_count: int,
+            level: int,
+            average_tries: int,
+            ko_shorts: list[str, ...],
     ):
         self.problem_id = problem_id
         self.title_ko = title_ko
@@ -91,5 +53,10 @@ class Problem:
             ko_shorts=ko_shorts,
         )
 
-    def get_embed(self):
-        return
+    @property
+    def rank(self) -> str:
+        return get_rank(level=self.level)
+
+    @property
+    def problem_url(self) -> str:
+        return get_problem_url(problem_id=self.problem_id)
