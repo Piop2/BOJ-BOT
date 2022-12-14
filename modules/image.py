53bb4432960c79391bb835ecd38422207176061d
Piop2 <seungyounyou0220@gmail.com>
2022-12-14 20:51:10 +0900

    problem UI show rank img

--- modules/image.py
+++ modules/image.py
@@ -7,6 +7,7 @@ from pygame import Surface
 from boj.problem import Problem
 
 SAVE_PATH = "temp/problem_thumbnail.png"
+RANK_IMG_PATH = "resource/rank/{}.png"
 
 pygame.font.init()
 
@@ -15,18 +16,27 @@ def make_problem_thumbnail(problem: Problem):
     surf = Surface((1280, 720))
     surf.fill((255, 255, 255))
     rank_image = pygame.transform.scale(
-        pygame.image.load("resource/rank/0.png"),
+        pygame.image.load(RANK_IMG_PATH.format(problem.level)),
         (200, 256)
     )
     id_font = pygame.font.SysFont(name="malgungothic", size=60, bold=True)
     id_text = id_font.render(str(problem.id), True, (0, 0, 0))
-    title_font = pygame.font.SysFont(name="malgungothic", size=210, bold=True)
+
+    title_font = pygame.font.SysFont(name="malgungothic", size=150, bold=True)
     title_text = title_font.render(problem.title, True, (0, 0, 0))
+
+    tag_font = pygame.font.SysFont(name="malgungothic", size=35, bold=True)
+    if len(problem.shorts) > 4:
+        tag_str = f"{' '.join([f'#{i}' for i in problem.shorts[:4]])} ..."
+    else:
+        tag_str = " ".join([f'#{i}' for i in problem.shorts])
+    tag_text = tag_font.render(tag_str, True, (100, 100, 100))
 
     surf.blit(rank_image, (50, 170))
     surf.blit(id_text, (150 - (id_text.get_width() / 2), 90))
-    surf.blit(title_text, (300, 298 - (title_text.get_height() / 2)))
-    pygame.draw.line(surf, (0, 0, 0), (50, 450), (1230, 450), 5)
+    surf.blit(title_text, (290, 298 - (title_text.get_height() / 2)))
+    surf.blit(tag_text, (290, 480 - tag_text.get_height()))
+    pygame.draw.line(surf, (0, 0, 0), (50, 480), (1230, 480), 5)
     pygame.image.save(surf, SAVE_PATH)
     return surf
 
