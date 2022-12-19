62c4374f79c1e1333ad7b2afb89e7ed37b25f132
Piop2 <seungyounyou0220@gmail.com>
2022-12-19 10:57:24 +0900

    merged /problem, /problem-img

--- cogs/problem_search.py
+++ cogs/problem_search.py
@@ -13,6 +13,7 @@ from discord import Interaction
 
 import solvedac
 from utils.logger import get_logger
+from modules.image import make_problem_thumbnail
 
 problem_log = get_logger("cmd.problem")
 
@@ -46,6 +47,33 @@ class SearchProblem(commands.Cog):
             embed=embed, file=rank_icon, ephemeral=False
         )
         problem_log.info(f"problem found: {problem_id}")
+        return
+
+    @app_commands.command(name="problem-img", description="search BOJ problem with ID")
+    @app_commands.describe(problem_id="problem ID registered on BOJ")
+    async def search(self, interaction: Interaction, problem_id: int) -> None:
+        try:
+            problem = solvedac.search_problem(problem_id=problem_id)
+        except solvedac.SolvedAcApiError.ProblemApiError.ProblemNotExistError:
+            await interaction.response.send_message(
+                f"ERROR problem id '{problem_id}' does not exist", ephemeral=False
+            )
+            problem_log.warning(f"problem does not exist: {problem_id}")
+            return
+
+        try:
+            make_problem_thumbnail(problem=problem)
+            file = File("temp/problem_thumbnail.png", filename="test_thumbnail.png")
+
+            embed = Embed(title="보러 가기", url=problem.url)
+            embed.set_image(url="attachment://test_thumbnail.png")
+
+            await interaction.response.send_message(
+                embed=embed, file=file, ephemeral=False
+            )
+            problem_log.info(f"problem found: {problem_id}")
+        except Exception as e:
+            problem_log.error(e)
         return
 
     @search.error
