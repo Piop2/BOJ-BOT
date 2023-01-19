d41330847b8c49ecff5b601893798882dd12ac54
Piop2 <seungyounyou0220@gmail.com>
2023-01-20 04:21:34 +0900

    remove background vars should not used

--- solvedac/background.py
+++ solvedac/background.py
@@ -3,15 +3,11 @@ class Background:
         self,
         background_id: str,
         background_image_url: str,
-        author: str,
-        author_url: str,
         display_name: str,
         display_description: str,
     ):
         self.id = background_id
         self.image_url = background_image_url
-        self.author = author
-        self.author_url = author_url
         self.name = display_name
         self.description = display_description
 
@@ -19,15 +15,12 @@ class Background:
     def load_json(cls, json: dict):
         background_id = json["backgroundId"]
         background_image_url = json["backgroundImageUrl"]
-        author = json["author"]
-        author_url = json["authorUrl"]
+        # authors = json["authors"]
         display_name = json["displayName"]
         display_description = json["displayDescription"]
         return cls(
             background_id=background_id,
             background_image_url=background_image_url,
-            author=author,
-            author_url=author_url,
             display_name=display_name,
-            display_description=display_description,
+            display_description=display_description
         )
